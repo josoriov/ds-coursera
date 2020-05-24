bfa155d6060f38a99c77728d8afaefc7a2338bdc
juanferov <juanferov97@gmail.com>
2020-05-24 17:35:25 -0500

    Class notes from 2020/05/23

---
+++ 2_rprogramming/w3/apply_functions.R
@@ -0,0 +1,17 @@
+# Apply function
+# lapply
+x <- list(a=1:5, b=norm(10))
+lapply(x, mean)
+lapply(x, runif, min=0, max=10) # Random uniform variable generator
+
+# apply
+x <- matrix(rnorm(200), 20, 10)
+apply(x, 1, sum) # Sum of rows
+apply(x, 2, mean) # Mean of columns
+# Note that the functions colSums, colMeans, rowSums and rowMeans exist
+apply(x, 1, quantile, probs=c(0.25,0.75)) # Calculate quantiles
+a <- array(rnorm(2*2*10), c(2,2,10))
+apply(a, c(1,2), mean) # Mean of first two dimensions
+
+# mapply
+mapply(rep, 1:4, 4:1)
